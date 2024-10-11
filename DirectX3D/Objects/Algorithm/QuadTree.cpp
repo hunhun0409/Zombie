1451ebedeb33d4f5147f54b3f61f6c1d8d6fb8a3
hunhun0409 <58500136+hunhun0409@users.noreply.github.com>
2024-10-11 21:21:15 +0900

    .

--- DirectX3D/Objects/Algorithm/QuadTree.cpp
+++ DirectX3D/Objects/Algorithm/QuadTree.cpp
@@ -7,6 +7,7 @@ QuadTree::QuadTree(Terrain* terrain)
     root->size = terrain->GetSize().x * terrain->Scale().x;
     root->x = root->size/2;
     root->z = root->size/2;
+    root->level = 0;
 }
 
 QuadTree::~QuadTree()
@@ -17,6 +18,39 @@ QuadTree::~QuadTree()
 
 void QuadTree::Update()
 {
+    UpdateTree(root);
+}
+
+void QuadTree::UpdateTree(Node* node)
+{
+    if (!node) return;
+
+    for (auto it = node->colliders.begin(); it != node->colliders.end();)
+    {
+        Collider* collider = *it;
+        if (!IsColliderInNode(node, collider))
+        {
+            it = node->colliders.erase(it);
+            Insert(collider);
+        }
+        else
+        {
+            it++;
+        }
+    }
+    FOR(4)
+    {
+        if (node->children[i])
+        {
+            UpdateTree(node->children[i]);
+        }
+    }
+
+    if (node->level > 0 && GetTotalColliders(node) <= MAX_OBJECT)
+    {
+        Merge(node);
+    }
+
 }
 
 void QuadTree::Render()
@@ -30,6 +64,7 @@ void QuadTree::Insert(Collider* collider)
 
 void QuadTree::Remove(Collider* collider)
 {
+    RemoveRecursive(root, collider);
 }
 
 void QuadTree::Split(Node* node)
@@ -38,31 +73,80 @@ void QuadTree::Split(Node* node)
     node->children[0] = new Node();
     node->children[0]->size = node->size / 2;
     node->children[0]->x = node->x - node->size/4;
-    node->children[0]->x = node->z + node->size/4;
+    node->children[0]->z = node->z + node->size/4;
+    node->children[0]->level = node->level + 1;
+    node->children[0]->parent = node;
+
     //RT
     node->children[1] = new Node();
     node->children[1]->size = node->size / 2;
     node->children[1]->x = node->x + node->size / 4;
-    node->children[1]->x = node->z + node->size / 4;
+    node->children[1]->z = node->z + node->size / 4;
+    node->children[1]->level = node->level + 1;
+    node->children[1]->parent = node;
+
     //LB
     node->children[2] = new Node();
     node->children[2]->size = node->size / 2;
     node->children[2]->x = node->x - node->size / 4;
-    node->children[2]->x = node->z - node->size / 4;
+    node->children[2]->z = node->z - node->size / 4;
+    node->children[2]->level = node->level + 1;
+    node->children[2]->parent = node;
+
     //RB
     node->children[3] = new Node();
     node->children[3]->size = node->size / 2;
     node->children[3]->x = node->x + node->size / 4;
-    node->children[3]->x = node->z - node->size / 4;
+    node->children[3]->z = node->z - node->size / 4;
+    node->children[3]->level = node->level + 1;
+    node->children[3]->parent = node;
+
+    //추가 : 분열되면 가지고 있는 collider 다 자식에게 넘김
+    for (Collider* collider : node->colliders)
+    {
+        int index = GetQuadrant(node, collider);
+        InsertRecursive(node->children[index], collider, node->level + 1);
+    }
 }
 
 void QuadTree::Merge(Node* node)
 {
+    if (node->level >= MAX_LEVEL)
+    {
+        for (Collider* collider : node->colliders)
+        {
+            InsertRecursive(node->parent, collider, node->level - 1);
+        }
+        return;
+    }
+    if (node->children[0] == nullptr)
+    {
+        for (Collider* collider : node->colliders)
+        {
+            InsertRecursive(node->parent, collider, node->level - 1);
+        }
+        return;
+    }
+
+    FOR(4)
+    {
+        if (node->children[i])
+        {
+            Merge(node->children[i]);
+        }
+    }
 }
 
-vector<Collider*> QuadTree::GetCollidersInArea(Node* node)
+
+int QuadTree::GetTotalColliders(Node* node)
 {
-    return vector<Collider*>();
+    int total = node->colliders.size();
+    for (int i = 0; i < 4; ++i) {
+        if (node->children[i]) {
+            total += GetTotalColliders(node->children[i]);
+        }
+    }
+    return total;
 }
 
 void QuadTree::DeleteNode(Node* node)
@@ -91,7 +175,7 @@ void QuadTree::InsertRecursive(Node* node, Collider* collider, int level)
         Split(node);
     }
 
-    //이미 분열됨
+    //분열됨
     if (node->children[0] != nullptr)
     {
         int index = GetQuadrant(node, collider);
@@ -101,8 +185,37 @@ void QuadTree::InsertRecursive(Node* node, Collider* collider, int level)
             return;
         }
     }
+    else
+    {
+        node->colliders.push_back(collider);
+    }
+}
 
-    node->colliders.push_back(collider);
+bool QuadTree::RemoveRecursive(Node* node, Collider* collider)
+{
+    if (!node)return false;
+
+    //이 노드에서 찾아본다
+    for (auto it = node->colliders.begin(); it != node->colliders.end(); it++)
+    {
+        if (*it == collider)
+        {
+            node->colliders.erase(it);
+            return true;
+        }
+    }
+
+    //이 노드에 없네? 자식 노드에 가서 찾는다
+    if (node->children[0])
+    {
+        int index = GetQuadrant(node, collider);
+        if (RemoveRecursive(node->children[index], collider))
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int QuadTree::GetQuadrant(Node* node, Collider* collider)
@@ -119,4 +232,14 @@ int QuadTree::GetQuadrant(Node* node, Collider* collider)
         if (center.z < node->z) return 1; // 우상단
         else return 3; // 우하단
     }
+}
+
+bool QuadTree::IsColliderInNode(Node* node, Collider* collider)
+{
+    if (collider->GlobalPos().x > node->x + node->size * 0.5f || collider->GlobalPos().x < node->x - node->size * 0.5f
+        || collider->GlobalPos().z > node->z + node->size * 0.5f || collider->GlobalPos().z < node->z - node->size * 0.5f)
+    {
+        return false;
+    }
+    return true;
 }
--- DirectX3D/Objects/Algorithm/QuadTree.h
+++ DirectX3D/Objects/Algorithm/QuadTree.h
@@ -9,8 +9,16 @@ private:
     struct Node
     {
         float x, z, size;
+        int level;
         vector<Collider*> colliders;
         Node* children[4] = {};
+        Node* parent = nullptr;
+    };
+
+    struct AABB
+    {
+        Vector3 minPos;
+        Vector3 maxPos;
     };
 
 public:
@@ -18,6 +26,7 @@ public:
 	~QuadTree();
 
     void Update();
+    void UpdateTree(Node* node);
     void Render();
 
     void Insert(Collider* collider);
@@ -26,13 +35,16 @@ public:
     void Split(Node* node);
     void Merge(Node* node);
 
-    vector<Collider*> GetCollidersInArea(Node* node);
+    int GetTotalColliders(Node* node);
 
 private:
     void DeleteNode(Node* node);
 
     void InsertRecursive(Node* node, Collider* collider, int level);
+    bool RemoveRecursive(Node* node, Collider* collider);
     int GetQuadrant(Node* node, Collider* collider);
+
+    bool IsColliderInNode(Node* node, Collider* collider);
 private:
     Node* root;
 };
